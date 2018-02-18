3131e33404f70449e8382201a2db8f366f8c6beb
Edward Huang <edwardhuang@EdwardmatoMacBook-Air.local>
2018-02-19 01:02:55 +0800

    Remove border line in iOS

---
+++ BottomBar.iOS/Renderers/BottomBarPageRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BottomBar.iOS.Renderers;
+using BottomBar.XamarinForms;
+using CoreGraphics;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BottomBarPage), typeof(BottomBarPageRenderer))]
+namespace BottomBar.iOS.Renderers
+{
+    public class BottomBarPageRenderer : TabbedRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            RemoveBorderLine();
+        }
+
+        protected virtual void RemoveBorderLine()
+        {
+            TabBar.ShadowImage = new UIImage();
+            TabBar.BackgroundImage = new UIImage();
+
+        }
+
+        public static void Initialize()
+        {
+            
+        }
+    }
+}
